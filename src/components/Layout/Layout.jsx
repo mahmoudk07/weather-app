60c0fc505ffe3c00ac343ef30f8e31c4ad8fd5c7
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-08-31 01:50:03 +0300

    ✅ add layout wrapper

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+import Footer from '../Footer/Footer'
+const Layout = ({ children }) => {
+  return (
+    <>
+        <Heading />
+        <main>{children}</main>
+        {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Layout
