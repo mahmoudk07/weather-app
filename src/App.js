0a4c2ae7f79dc7167b364b3af698577cb3f847c4
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-08-30 18:42:09 +0300

    ✅ make test in async calls in app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCurrentWeather } from "./services/weatherInfo/weatherSlice";
 function App() {
+  const { data, loading, error } = useSelector(state => state.Weather);
+  const dispatch = useDispatch();
+  const fetchWeatherData = async () => {
+    try {
+      await dispatch(fetchCurrentWeather()).unwrap();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchWeatherData();
+  }, [dispatch]);
   return (
     <div className="App">
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {!loading && data && <h1>{data.data.weather[0].date}</h1>}
     </div>
   );
 }
