e533b3b04fc902d673a434891a50bfd335c03dab
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-08-31 01:49:44 +0300

    ✅ modify app structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCurrentWeather } from "./services/weatherInfo/weatherSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingDisplay from "./pages/LandingDisplay/LandingDisplay";
+import CityDashboard from "./pages/CityDashboard/CityDashboard"
 function App() {
-  const { data, loading, error } = useSelector(state => state.Weather);
-  const dispatch = useDispatch();
-  const fetchWeatherData = async () => {
-    try {
-      await dispatch(fetchCurrentWeather()).unwrap();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(() => {
-    fetchWeatherData();
-  }, [dispatch]);
   return (
-    <div className="App">
-      {loading && <h1>Loading...</h1>}
-      {error && <h1>{error}</h1>}
-      {!loading && data && <h1>{data.data.weather[0].date}</h1>}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingDisplay />} />
+        <Route path="/city/:cityName" element={<CityDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
