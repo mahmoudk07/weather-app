0bcf51a2724e9c68eff38d3667a063a14fad8970
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-08-31 01:49:52 +0300

    🔒️ add heading

---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div className = 'w-full h-28 flex justify-center items-center font-bold text-[30px]'>
+        <h1 className = 'text-white'>Weather App</h1>
+    </div>
+  )
+}
+
+export default Heading
