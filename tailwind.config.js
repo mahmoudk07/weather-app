9dfe65813567b6c3a82f6c35837ea7a15160203d
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-02 19:06:05 +0300

    ✅ add more configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
   theme: {
     extend: {
       backgroundColor: {
-        "weatherCard-bg": 'rgba(47, 53, 64, 255)',
+        "weatherCard-bg": "rgba(47, 53, 64, 255)",
       },
       screens: {
-        'xs': '470px',
+        xs: "470px",
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
-
