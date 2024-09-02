b8d8c3f3b91a034af3e4bd245442009eb176dc51
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-03 02:24:01 +0300

    ✅ Add City dashboard page

--- src/pages/CityDashboard/CityDashboard.jsx
+++ src/pages/CityDashboard/CityDashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import WeatherInfo from '../LandingDisplay/components/WeatherInfo'
+import WeatherForecast from '../LandingDisplay/components/WeatherForecast'
+import LineChart from '../../components/LineChart/LineChart'
+import RainfallChart from "../../components/LineChart/RainfallChart"
+import { dummyClimateData } from '../../data'
 const CityDashboard = () => {
   return (
-    <div>
-      
+    <div className='w-full min-h-[90vh] px-[3%] py-[1%]'>
+      <div className = 'flex xs:flex-col lg:flex-row md:items-center items-start gap-5 mb-5 justify-around'>
+        <WeatherInfo />
+        <LineChart data={dummyClimateData} />
+      </div>
+      <div className = 'flex xs:flex-col lg:flex-row md:items-center items-start gap-5 justify-around'>
+        <WeatherForecast />
+        <RainfallChart data={dummyClimateData} />
+      </div>
     </div>
   )
 }
