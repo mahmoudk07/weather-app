adade4bd0687225c5af81f37b47dcf8d80b3c46d
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-02 21:51:36 +0300

    ✅ apply responsivenss

--- src/pages/LandingDisplay/components/WeatherForecast.jsx
+++ src/pages/LandingDisplay/components/WeatherForecast.jsx
@@ -3,11 +3,11 @@ import WeatherDay from './WeatherDay'
 
 const WeatherForecast = () => {
   return (
-    <div className= 'xs:w-[100%] sm:w-[100%] md:w-[85%] lg:w-[70%] xl:w-[48%] min-h-[26vh] bg-[#071a33] rounded-[15px] px-[3%] py-[1.5%]'>
+    <div className= 'xs:w-[100%] sm:w-[100%] md:w-[85%] lg:w-[70%] xl:w-[48%] min-h-[26vh] bg-[#071a33] rounded-[15px] px-[3%] py-[3%]'>
         <header>
             <h1 className= 'text-[16px] text-[#1d5a8b] font-semibold mb-4'>Weather's Forecast</h1>
         </header>
-      <div className = 'flex items-center  xl:gap-x-8 flex-wrap gap-y-8'>
+      <div className = 'flex items-center xs:justify-center xs:gap-x-20 sm:gap-x-16 xs:px-[12%] sm:px-[0%] xl:gap-x-12 flex-wrap gap-y-8'>
         <WeatherDay />
         <WeatherDay />
         <WeatherDay />
