b65730403848e06528796abc36b79a68c0d42aed
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-02 19:06:30 +0300

    ✅ update dummy data file

---
+++ src/data.js
@@ -0,0 +1,35 @@
+// data.js
+export const dummyClimateData = [
+  { name: "Jan", avgMinTemp: 2.5, absMaxTemp: 9.5, avgDailyRainfall: 1.8 },
+  {
+    name: "Feb",
+    avgMinTemp: 3.0,
+    absMaxTemp: 10.0,
+    avgDailyRainfall: 1.9,
+  },
+  { name: "Mar", avgMinTemp: 4.5, absMaxTemp: 13.0, avgDailyRainfall: 1.6 },
+  { name: "Apr", avgMinTemp: 7.0, absMaxTemp: 17.5, avgDailyRainfall: 1.4 },
+  { name: "May", avgMinTemp: 10.0, absMaxTemp: 21.0, avgDailyRainfall: 1.7 },
+  { name: "Jun", avgMinTemp: 13.0, absMaxTemp: 25.0, avgDailyRainfall: 2.3 },
+  { name: "Jul", avgMinTemp: 15.0, absMaxTemp: 27.0, avgDailyRainfall: 2.0 },
+  { name: "Aug", avgMinTemp: 15.5, absMaxTemp: 26.5, avgDailyRainfall: 2.2 },
+  {
+    name: "Sep",
+    avgMinTemp: 13.0,
+    absMaxTemp: 23.5,
+    avgDailyRainfall: 1.5,
+  },
+  { name: "Oct", avgMinTemp: 9.0, absMaxTemp: 18.0, avgDailyRainfall: 2.4 },
+  {
+    name: "Nov",
+    avgMinTemp: 5.0,
+    absMaxTemp: 13.0,
+    avgDailyRainfall: 2.2,
+  },
+  {
+    name: "Dec",
+    avgMinTemp: 3.0,
+    absMaxTemp: 10.0,
+    avgDailyRainfall: 2.1,
+  },
+];
