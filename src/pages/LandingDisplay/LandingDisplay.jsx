5ddd415239e68beffddedabe52e5a9511ed5f1c0
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-02 19:07:39 +0300

    ✅ modify landin page

--- src/pages/LandingDisplay/LandingDisplay.jsx
+++ src/pages/LandingDisplay/LandingDisplay.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCurrentWeather } from "../../services/weatherInfo/weatherSlice"
-import WeatherSummary from './components/WeatherSummary';
-import WeatherMetrics from './components/WeatherMetrics'
+import { Spinner } from "flowbite-react";
+// import WeatherSummary from './components/WeatherSummary';
+// import WeatherMetrics from './components/WeatherMetrics'
+// import LineChart from "../../components/LineChart/LineChart"
+// import RainfallChart from '../../components/LineChart/RainfallChart.jsx';
+// import { dummyClimateData } from "../../data.js"
+import WeatherInfo from './components/WeatherInfo.jsx';
+import WeatherForecast from './components/WeatherForecast.jsx';
 const LandingDisplay = () => {
-    const { data, loading, error } = useSelector(state => state.Weather);
+    const { currentWeather, loading, error } = useSelector(state => state.Weather);
     const dispatch = useDispatch();
     const fetchWeatherData = async () => {
         try {
@@ -15,12 +21,32 @@ const LandingDisplay = () => {
     }
     useEffect(() => {
         fetchWeatherData();
+        // console.log(dummyClimateData)
     }, [dispatch]);
   return (
-    <div className = 'landing-container'>
-        <WeatherSummary data={data} />
-        <WeatherMetrics data={data} />
+    <div className='flex flex-col items-center w-full min-h-[70vh] px-[10%] gap-y-5'>
+      {loading ? <div className='fixed inset-0 flex items-center justify-center bg-opacity-50 z-50'>
+        <Spinner color="info" size="5xl" className="h-12 w-12" />
+      </div> : ''}
+      {!loading && currentWeather ? 
+        <>
+          <WeatherInfo />
+          <WeatherForecast />
+        </> : ''
+      }
     </div>
   )
 }
 export default LandingDisplay
+
+    // <div className = 'landing-container'>
+    //     {/* <WeatherSummary data={data} />
+    //     <WeatherMetrics data={data} /> */}
+     
+    //   </div>
+        // <div className="w-full flex justify-center">
+        //   {/* <h3 className="text-xl mb-4">Temperature Trends</h3> */}
+        //     <LineChart data={dummyClimateData} />
+        //     {/* <h3 className="text-xl mb-4">Rainfall Trends</h3> */}
+        //   <RainfallChart data={dummyClimateData} />
+        // </div>  
