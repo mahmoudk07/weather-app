9170a8eea3ef7709b68f7230e5609ed8d0183ad0
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-02 19:08:05 +0300

    ✅ add weather day component

---
+++ src/pages/LandingDisplay/components/WeatherDay.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import WeatherIcon from "../../../assets/01d.png"
+const WeatherDay = () => {
+  return (
+    <div className = 'flex flex-col items-center w-[10%]'>
+        <p className= 'font-bold text-[#3774b5]'>Sat</p>
+        <figure className='w-[60px] h-[60px]'>
+            <img className='w-[100%] h-[100%]' src={WeatherIcon} alt="current weather status" />
+            <figcaption className='sr-only'>Weather icon representing current conditions</figcaption>
+        </figure>
+        <p className= 'font-bold text-[#46668f] mb-1'>Sunny</p>
+        <p className= 'font-semibold text-[#46668f] text-[14px]'>39°C/20°C</p>
+    </div>
+  )
+}
+
+export default WeatherDay
