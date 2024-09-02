6c53ab0bf098ada1e07637475cd965c33f9c42fc
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-03 02:24:29 +0300

    ⚡️ modify custom hook

--- src/hooks/useResizeObserver.js
+++ src/hooks/useResizeObserver.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 
 const useResizeObserver = (ref) => {
-  const [dimensions, setDimensions] = useState({ width: 400, height: 200 });
+  const [dimensions, setDimensions] = useState({ width: 500, height: 200 });
 
   useEffect(() => {
     const observeTarget = ref.current;
     const resizeObserver = new ResizeObserver((entries) => {
       entries.forEach((entry) => {
         const { width } = entry.contentRect;
-        const height = width * 0.5; // Aspect ratio 2:1
+        const height = width * 0.55; // Aspect ratio 2:1
         setDimensions({ width, height });
       });
     });
