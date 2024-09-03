ef5c3e1a286b45498302bf3fc36529c586f69fd3
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-03 20:14:21 +0300

    ✅ modify city dashboard

--- src/pages/CityDashboard/CityDashboard.jsx
+++ src/pages/CityDashboard/CityDashboard.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useEffect, useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Spinner } from "flowbite-react";
+import { fetchCurrentWeather } from '../../services/weatherInfo/weatherSlice'
 import WeatherInfo from '../LandingDisplay/components/WeatherInfo'
 import WeatherForecast from '../LandingDisplay/components/WeatherForecast'
 import LineChart from '../../components/LineChart/LineChart'
 import RainfallChart from "../../components/LineChart/RainfallChart"
-import { dummyClimateData } from '../../data'
+// import { dummyClimateData } from '../../data'
 const CityDashboard = () => {
+  const { loading, currentWeather, forecastWeather, historicalWeather } = useSelector(state => state.Weather)
+  const dispatch = useDispatch();
+  const fetchWeatherData = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentWeather()).unwrap();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
+  useEffect(() => {
+    // eslint-disable-next-line
+    fetchWeatherData();
+  }, [fetchWeatherData]);
   return (
     <div className='w-full min-h-[90vh] px-[3%] py-[1%]'>
-      <div className = 'flex xs:flex-col lg:flex-row md:items-center items-start gap-5 mb-5 justify-around'>
-        <WeatherInfo />
-        <LineChart data={dummyClimateData} />
-      </div>
-      <div className = 'flex xs:flex-col lg:flex-row md:items-center items-start gap-5 justify-around'>
-        <WeatherForecast />
-        <RainfallChart data={dummyClimateData} />
-      </div>
+      {loading ? <div className='spinner-container'>
+        <Spinner color="info" size="5xl" className="h-12 w-12" />
+      </div> : ''}
+      {!loading && currentWeather ? 
+        <>
+          <div className='flex xs:flex-col lg:flex-row md:items-center items-start gap-5 mb-5 justify-around'>
+            <WeatherInfo data={currentWeather} forecastWeather={forecastWeather[0]} />
+            <LineChart data={historicalWeather} />
+          </div>
+          <div className='flex xs:flex-col lg:flex-row md:items-center items-start gap-5 justify-around'>
+            <WeatherForecast forecastWeather={forecastWeather} />
+            <RainfallChart data={historicalWeather} />
+          </div>
+        </>
+      : ''}
     </div>
   )
 }
-
 export default CityDashboard
