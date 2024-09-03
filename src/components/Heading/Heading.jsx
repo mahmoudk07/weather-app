4b1eb3a3f06935485d394bbe80fd6d044bb544f8
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-03 16:43:34 +0300

    🐛 fix style

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Select from '../Select/Select'
+import WeatherLogo from "../../assets/weather-app.png"
 const Heading = () => {
   return (
-    <div className = 'header'>
-      <h1 className='text-white text-[30px]'>Weather App</h1>
+    <div className='header'>
+      <div className='flex items-center gap-x-3 px-[7%]'>
+        <img width={40} height={40} src={WeatherLogo} alt="weather logo" />
+        <h1 className='text-[#2e5d8b] text-[20px] justify-start'>Weather Forecast</h1>
+      </div>
       <Select />
     </div>
   )
