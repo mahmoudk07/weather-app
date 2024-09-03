f273f9c4e6a4596417360a977f417e01ef77f44c
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-03 20:14:32 +0300

    ✅ fix style

--- src/pages/LandingDisplay/components/WeatherDay.jsx
+++ src/pages/LandingDisplay/components/WeatherDay.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import WeatherIcon from "../../../assets/01d.png"
+import { getWeatherStatusImage } from "../../../utils/getWeatherStatusImage"
 const WeatherDay = ({key, day, forecastWeather}) => {
   return (
     <div className= 'weather-day-container' key = {key}>
       <p className='weather-day'>{day}</p>
       <figure className='w-[60px] h-[60px]'>
-          <img className='w-[100%] h-[100%]' src={WeatherIcon} alt="current weather status" />
+          <img className='w-[100%] h-[100%]' src={getWeatherStatusImage(forecastWeather.hourly[2].weatherDesc[0].value)} alt="current weather status" />
           <figcaption className='sr-only'>Weather icon representing current conditions</figcaption>
       </figure>
       <p className='weather-day-status'>{forecastWeather.hourly[2].weatherDesc[0].value }</p>
