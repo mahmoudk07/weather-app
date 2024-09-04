d5c9348972298fa22d07ce7236397ff26ec73557
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 18:38:12 +0300

    ✅ add header links

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import WeatherLogo from "../../assets/weather-app.png"
+import { Link } from 'react-router-dom'
 const Heading = () => {
   return (
     <div className='header'>
@@ -7,6 +8,10 @@ const Heading = () => {
         <img width={40} height={40} src={WeatherLogo} alt="weather logo" />
         <h1 className='header-text'>Weather Forecast</h1>
       </div>
+      <nav className = 'flex items-center gap-x-6'>
+        <Link to='/' className='text-[#2e5d8b] font-bold'>Home</Link>
+        <Link to='/city' className='text-[#2e5d8b] font-bold'>Dashboard</Link>
+      </nav>
     </div>
   )
 }
