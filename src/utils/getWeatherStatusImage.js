2736000422853a9442a56d35731fe3ab14e7ef3b
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 18:03:25 +0300

    fix error

--- src/utils/getWeatherStatusImage.js
+++ src/utils/getWeatherStatusImage.js
@@ -1,28 +1,29 @@
-import Sunny from "../assets/Sunny.png"
-import Clear from "../assets/Clear.png"
-import Cloudy from "../assets/Cloudy.png"
-import Rainy from "../assets/Rainy.png"
-import Snowy from "../assets/Snowy.png"
-import PartlyCloudy from "../assets/Partly Cloudy.png"
-import Thunderstorm from "../assets/Thunderstorm.png"  
-export const getWeatherStatusImage = (status) => { 
-    const lowerCaseStatus = status.toLowerCase();
-    switch (lowerCaseStatus) {
-      case "sunny":
-        return Sunny;
-      case "clear":
-        return Clear;
-      case "cloudy":
-        return Cloudy;
-      case "partly cloudy":
-        return PartlyCloudy;
-      case lowerCaseStatus.includes("rain"):
-        return Rainy;
-      case lowerCaseStatus.includes("snow"):
-        return Snowy;
-      case lowerCaseStatus.includes("thunder"):
-        return Thunderstorm;
-      default:
-        return Clear;
-    }
-}
+import Sunny from "../assets/Sunny.png";
+import Clear from "../assets/Clear.png";
+import Cloudy from "../assets/Cloudy.png";
+import Rainy from "../assets/Rainy.png";
+import Snowy from "../assets/Snowy.png";
+import PartlyCloudy from "../assets/Partly Cloudy.png";
+import Thunderstorm from "../assets/Thunderstorm.png";
+
+export const getWeatherStatusImage = (status) => {
+  const lowerCaseStatus = status.toLowerCase();
+  switch (true) {
+    case lowerCaseStatus === "sunny":
+      return Sunny;
+    case lowerCaseStatus === "clear":
+      return Clear;
+    case lowerCaseStatus === "cloudy":
+      return Cloudy;
+    case lowerCaseStatus === "partly cloudy":
+      return PartlyCloudy;
+    case lowerCaseStatus.includes("rain"):
+      return Rainy;
+    case lowerCaseStatus.includes("snow"):
+      return Snowy;
+    case lowerCaseStatus.includes("thunder"):
+      return Thunderstorm;
+    default:
+      return Clear;
+  }
+};
