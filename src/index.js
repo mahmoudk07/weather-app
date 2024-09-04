3ced4c042ad83d289346b2c6be16afef340f602a
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 18:38:02 +0300

    ✅ refactor index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
-import Layout from './components/Layout/Layout';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Layout>
       <App />
-    </Layout>
   </Provider>
 );
