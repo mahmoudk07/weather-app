4e732b040faa303a5f3c0d9c6c27338ce70a9376
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 22:20:57 +0300

    ✅ clean landing page

--- src/pages/LandingDisplay/LandingDisplay.jsx
+++ src/pages/LandingDisplay/LandingDisplay.jsx
@@ -1,36 +1,26 @@
-import React, { useEffect, useCallback } from 'react'
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCurrentWeather } from "../../services/weatherInfo/weatherSlice"
+import React, { useEffect } from 'react'
+import useWeatherData from '../../hooks/useWeatherData.js';
 import { Spinner } from "flowbite-react";
 import Select from "../../components/Select/Select.jsx"
 import WeatherSummary from '../../components/Weather/WeatherSummary/WeatherSummary.jsx';
 import WeatherForecast from '../../components/Weather/WeatherForecast/WeatherForecast.jsx';
 const LandingDisplay = () => {
-    const { currentWeather, forecastWeather, loading, city } = useSelector(state => state.Weather);
-    const dispatch = useDispatch();
-    const fetchWeatherData = useCallback(async (city) => {
-      try {
-        await dispatch(fetchCurrentWeather(city)).unwrap();
-      } catch (error) {
-        console.log(error);
-      }
-    }, [dispatch]);
-  useEffect(() => {
-    // eslint-disable-next-line
-      fetchWeatherData();
-  }, [fetchWeatherData]);
+    const { currentWeather, forecastWeather, loading, city, fetchWeatherData } = useWeatherData();
+    useEffect(() => {
+        fetchWeatherData();
+    }, [fetchWeatherData]);
   return (
       <div className='landing-container'>
         <Select onSelectChange = {fetchWeatherData}  />
-        {loading ? <div className='spinner-container'>
+        {loading && ( <div className='spinner-container'>
           <Spinner color="info" size="5xl" className="h-12 w-12" />
-        </div> : ''}
-        {!loading && currentWeather ?
+        </div> )}
+        {!loading && currentWeather && (
           <>
             <WeatherSummary data={currentWeather} forecastWeather={forecastWeather[0]} currentCity = {city} />
             <WeatherForecast forecastWeather={forecastWeather} />
-          </> : ''
-        }
+          </>
+      )}
       </div>
   )
 }
