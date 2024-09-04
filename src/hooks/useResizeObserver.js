204e3190377726e5b8f3293193ad637e9e163067
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-05 01:43:12 +0300

    ✅ optimize useResizeObserver hook

--- src/hooks/useResizeObserver.js
+++ src/hooks/useResizeObserver.js
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const useResizeObserver = (ref) => {
   const [dimensions, setDimensions] = useState({ width: 470, height: 200 });
 
+  // Callback function for ResizeObserver
+  const handleResize = useCallback((entries) => {
+    entries.forEach((entry) => {
+      const { width } = entry.contentRect;
+      const height = width * 0.55; 
+      setDimensions({ width, height });
+    });
+  }, []);
+
   useEffect(() => {
     const observeTarget = ref.current;
-    const resizeObserver = new ResizeObserver((entries) => {
-      entries.forEach((entry) => {
-        const { width } = entry.contentRect;
-        const height = width * 0.55; // Aspect ratio 2:1
-        setDimensions({ width, height });
-      });
-    });
+    const resizeObserver = new ResizeObserver(handleResize);
 
     if (observeTarget) {
       resizeObserver.observe(observeTarget);
@@ -22,7 +25,7 @@ const useResizeObserver = (ref) => {
         resizeObserver.unobserve(observeTarget);
       }
     };
-  }, [ref]);
+  }, [ref, handleResize]);
 
   return dimensions;
 };
