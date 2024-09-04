01c1a7899a935a80c0076b90ef7e79880c759186
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 22:20:25 +0300

    ✅ remove unneeded lines

--- src/components/Weather/WeatherDay/WeatherDay.jsx
+++ src/components/Weather/WeatherDay/WeatherDay.jsx
@@ -8,7 +8,6 @@ const WeatherDay = ({key, day, forecastWeather}) => {
           <img className='w-[100%] h-[100%]' src={getWeatherStatusImage(forecastWeather.hourly[2].weatherDesc[0].value)} alt="current weather status" />
           <figcaption className='sr-only'>Weather icon representing current conditions</figcaption>
       </figure>
-      {/* <p className='weather-day-status'>{forecastWeather.hourly[2].weatherDesc[0].value }</p> */}
       <p className='weather-day-temps'>{forecastWeather.maxtempC}°C/{forecastWeather.mintempC }°C</p>
     </div>
   )
