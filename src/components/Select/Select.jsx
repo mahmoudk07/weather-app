2c41f4a355571bb51bf5b9479645b41d70d3837b
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 22:20:12 +0300

    ⚡️ clean select component

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -3,7 +3,8 @@ import { useSelector } from 'react-redux'
 const Select = ({ onSelectChange }) => {
   const { cities } = useSelector(state => state.Weather)
   const handleChange = (e) => {
-    onSelectChange(e.target.value)
+    const city = e.target.value
+    onSelectChange(city)
   }
   return (
     <select className='select' onChange={handleChange}>
