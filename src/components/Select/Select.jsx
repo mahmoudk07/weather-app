28b21aa0323c0b4e64b099db7375250991e622d0
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-05 02:18:22 +0300

    ✅ update select component

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { changeSelectedCity } from "../../services/weatherInfo/weatherSlice"
 const Select = ({ onSelectChange }) => {
-  const { cities } = useSelector(state => state.Weather)
+  const { cities, city } = useSelector(state => state.Weather)
+  const dispatch = useDispatch()
   const handleChange = (e) => {
     const city = e.target.value
+    dispatch(changeSelectedCity(city))
     onSelectChange(city)
   }
   return (
-    <select className='select' onChange={handleChange}>
-        <option value="" disabled selected hidden>
+    <select className='select' onChange={handleChange} value={city || ""}>
+       <option disabled selected={ city ? false : true} hidden>
             Select a city
         </option>
         {cities?.map(city => (
