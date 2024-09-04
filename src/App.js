3d250f9336da670fb6dff2f84211453bd0e590ac
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 18:37:40 +0300

    ✅ modify app structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingDisplay from "./pages/LandingDisplay/LandingDisplay";
 import CityDashboard from "./pages/CityDashboard/CityDashboard"
+import Layout from "./components/Layout/Layout";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingDisplay />} />
-        <Route path="/city" element={<CityDashboard />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingDisplay />} />
+          <Route path="/city" element={<CityDashboard />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
