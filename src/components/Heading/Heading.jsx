4780ac0dc627715ca6525c87c1fe9ffbe1a15eae
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 04:28:42 +0300

    ✅ modify header

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Select from '../Select/Select'
 import WeatherLogo from "../../assets/weather-app.png"
 const Heading = () => {
   return (
@@ -8,7 +7,6 @@ const Heading = () => {
         <img width={40} height={40} src={WeatherLogo} alt="weather logo" />
         <h1 className='text-[#2e5d8b] text-[20px] justify-start'>Weather Forecast</h1>
       </div>
-      <Select />
     </div>
   )
 }
