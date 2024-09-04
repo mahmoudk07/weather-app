5ce76b032ca40815828765e95fee0fcb97838e8d
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-04 22:20:39 +0300

    ✨ add new custom hook to fetch weather data

---
+++ src/hooks/useWeatherData.js
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCurrentWeather } from "../services/weatherInfo/weatherSlice";
+
+const useWeatherData = () => {
+  const { currentWeather, forecastWeather, loading, city, historicalWeather } = useSelector((state) => state.Weather);
+  const dispatch = useDispatch();
+    
+  // fetch weather based on selected ciy
+  const fetchWeatherData = useCallback(
+    async (city) => {
+      try {
+        await dispatch(fetchCurrentWeather(city)).unwrap();
+      } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+      }
+    },
+    [dispatch]
+  );
+  return {
+    currentWeather,
+    forecastWeather,
+    loading,
+    city,
+    historicalWeather,
+    fetchWeatherData,
+  };
+};
+export default useWeatherData;
