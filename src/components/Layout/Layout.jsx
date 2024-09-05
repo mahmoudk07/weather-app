a1bffca5ceb290d634f4551659070fdf6544e76a
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-05 23:09:04 +0300

    ✅ clean layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
-// import Footer from '../Footer/Footer'
+import Footer from '../Footer/Footer'
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className = 'flex flex-col min-h-screen'>
         <Heading />
-        <main>{children}</main>
-        {/* <Footer /> */}
-    </>
+        <main className = 'flex-grow'>{children}</main>
+        <Footer />
+    </div>
   )
 }
 
