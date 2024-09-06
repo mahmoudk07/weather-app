fbf0729b464afcd91e64cea98cb358b0f5224701
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-06 20:39:15 +0300

    ✅ modify style

--- src/components/Charts/utils/setupSvg.js
+++ src/components/Charts/utils/setupSvg.js
@@ -2,5 +2,5 @@ export const setupSvg = (svg, width, height) => {
      svg
     .attr("width", width)
     .attr("height", height)
-    .classed("xss:w-[375px] xs:w-[440px]", true)
+    .classed("xss:w-[375px] xs:w-[440px] md:w-[470px]", true)
 };
