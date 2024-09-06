12e66e8dce578fa1e68ef1711091fb356ba97920
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-06 18:51:58 +0300

    ✅ add class to header logo

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -5,7 +5,7 @@ const Heading = () => {
   return (
     <div className='header'>
       <div className='header-logo'>
-        <img width={40} height={40} src={WeatherLogo} alt="weather logo" />
+        <img className = 'logo' width={40} height={40} src={WeatherLogo} alt="weather logo" />
         <h1 className='header-text'>Weather Forecast</h1>
       </div>
       <nav className = 'header-links'>
