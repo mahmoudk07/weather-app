259d2b4328eeaea0b22a685949d1890f122813f0
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-09-06 16:10:22 +0300

    solve error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer className='footer'>
       <p className = 'footer-text'>© 2024 This website is designed and developed by Mahmoud Khaled —</p>
       <div className = 'footer-icons'>
-        <FaGithub />
-        <FaLinkedin />
+        <Link to= "https://github.com/mahmoudk07" target='_blank'><FaGithub /></Link>
+        <Link to= "https://www.linkedin.com/feed/" target='_blank'><FaLinkedin /></Link>
       </div>
     </footer>
   )
